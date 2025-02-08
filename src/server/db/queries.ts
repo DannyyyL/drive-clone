dc8ed7eeecd1ca3de1e3d9bcfd212bbca55feb7c
Dan Lichtin <lichtindan@gmail.com>
2025-02-08 16:25:59 -0500

    Abstracting data access

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,40 +1,40 @@
+import "server-only";
+
 import {db} from "~/server/db";
 import {files_table as filesSchema, folders_table as foldersSchema} from "~/server/db/schema";
 import { eq } from "drizzle-orm";
 
-export async function getAllParentsForFolder(folderId: number) {
-  const parents = [];
-  let currentId: number | null = folderId;
-  while (currentId !== null) {
-    const folder = await db
+export const QUERIES = {
+  getFolders: function(folderId: number){
+    return db
       .select()
       .from(foldersSchema)
-      .where(eq(foldersSchema.id, currentId));
+      .where(eq(foldersSchema.id, folderId));//selecting where folder schema id is equal to folder id
+  },
+  getFiles: function(folderId: number){
+    return db
+      .select()
+      .from(filesSchema)
+      .where(eq(filesSchema.id, folderId));
 
-    if (!folder[0]){
-      throw new Error("Parent folder not found");
+  },
+  getAllParentsForFolder: async function(folderId: number) {
+    const parents = [];
+    let currentId: number | null = folderId;
+    while (currentId !== null) {
+      const folder = await db
+        .select()
+        .from(foldersSchema)
+        .where(eq(foldersSchema.id, currentId));
+
+      if (!folder[0]){
+        throw new Error("Parent folder not found");
+      }
+
+      parents.unshift(folder[0]);
+      currentId = folder[0]?.parent;
     }
 
-    parents.unshift(folder[0]);
-    currentId = folder[0]?.parent;
-  }
-
-  return parents;
-}
-
-export function getFolders(folderId: number){
-  return db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.id, folderId));//selecting where folder schema id is equal to folder id
-}
-
-export function getFiles(folderId: number){
-  return db
-    .select()
-    .from(filesSchema)
-    .where(eq(filesSchema.id, folderId));
-
-}
-
-
+    return parents;
+  },
+};
