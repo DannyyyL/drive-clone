8058ab22f8d7bcd475399a50bd64dfb7a5542e71
Dan Lichtin <lichtindan@gmail.com>
2025-02-08 16:17:44 -0500

    Seperating sql queries from the tsx file

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,27 +1,5 @@
-import {db} from "~/server/db";
-import {files_table as filesSchema, folders_table as foldersSchema} from "~/server/db/schema";
 import DriveContents from "../../drive-contents";
-import { eq } from "drizzle-orm";
-
-async function getAllParents(folderId: number) {
-  const parents = [];
-  let currentId: number | null = folderId;
-  while (currentId !== null) {
-    const folder = await db
-      .select()
-      .from(foldersSchema)
-      .where(eq(foldersSchema.id, currentId));
-
-    if (!folder[0]){
-      throw new Error("Parent folder not found");
-    }
-
-    parents.unshift(folder[0]);
-    currentId = folder[0]?.parent;
-  }
-
-  return parents;
-}
+import { getAllParentsForFolder, getFiles, getFolders } from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string}>;
@@ -32,21 +10,12 @@ export default async function GoogleDriveClone(props: {
   if (isNaN(parsedFolderId)) {
     return <div>Invalid folder ID</div>;
   } 
-
-  const foldersPromise = db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.id, parsedFolderId));//selecting where folder schema id is equal to folder id
-
-  const filesPromise = db
-    .select()
-    .from(filesSchema)
-    .where(eq(filesSchema.id, parsedFolderId));
-
-
-  const parentsPromise= getAllParents(parsedFolderId);
-
-  const[folders,files, parents] = await Promise.all([foldersPromise,filesPromise,parentsPromise]);
+  
+  const[folders,files, parents] = await Promise.all([
+    getFolders(parsedFolderId),
+    getFiles(parsedFolderId),
+    getAllParentsForFolder(parsedFolderId),
+  ]);
 
   return <DriveContents files={files} folders={folders} parents={parents}/>
 }
