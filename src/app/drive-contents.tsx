6ae10d37b2e6328f27f1754ee3f12bdcf6b07cfa
Dan Lichtin <lichtindan@gmail.com>
2025-02-08 16:05:41 -0500

    Fixing breadcrumbs

--- src/app/drive-contents.tsx
+++ src/app/drive-contents.tsx
@@ -1,7 +1,5 @@
-"use client"
+"use client";
 
-import { useState, useMemo } from "react"
-import { mockFiles, mockFolders } from "../lib/mock-data"
 import { Upload, ChevronRight } from "lucide-react"
 import { Button } from "~/components/ui/button"
 import { FileRow, FolderRow } from "./file-row"
@@ -9,15 +7,13 @@ import type { files, folders } from "../server/db/schema";
 import Link from "next/link"
 
 export default function GoogleDriveClone(props: {
-  files: typeof files.$inferSelect[];
-  folders: typeof folders.$inferSelect[];
+  files: (typeof files.$inferSelect)[];
+  folders: (typeof folders.$inferSelect)[];
+  parents: (typeof folders.$inferSelect)[];
 }) {
-
-  const breadcrumbs: unknown[] = [];
-
   const handleUpload = () => {
     alert("Upload functionality would be implemented here")
-  }
+  };
 
   return (
     <div className="min-h-screen bg-gray-900 text-gray-100 p-8">
@@ -27,7 +23,7 @@ export default function GoogleDriveClone(props: {
             <Link href="/f/1" className="text-gray-300 hover:text-white mr-2">
               My Drive
             </Link>
-            {breadcrumbs.map((folder, index) => (
+            {props.parents.map((folder, index) => (
               <div key={folder.id} className="flex items-center">
                 <ChevronRight className="mx-2 text-gray-500" size={16} />
                 <Link
