deb1a1eda74c2a187188eeb44bca505cb7473114
Dan Lichtin <lichtindan@gmail.com>
2025-03-03 23:59:02 -0500

    Color background options

--- src/app/f/[folderId]/drive-contents.tsx
+++ src/app/f/[folderId]/drive-contents.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Upload, ChevronRight } from "lucide-react"
+import { ChevronRight, Circle } from "lucide-react"
 import { FileRow, FolderRow } from "./file-row"
 import type { files_table, folders_table } from "~/server/db/schema";
 import Link from "next/link"
 import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
 import { UploadButton } from "~/components/uploadthing";
 import { useRouter } from "next/navigation";
+import { useState } from "react"
 
 export default function GoogleDriveClone(props: {
   files: (typeof files_table.$inferSelect)[];
@@ -16,11 +17,24 @@ export default function GoogleDriveClone(props: {
 }) {
   const navigate = useRouter();
 
+  const [color, setColor] = useState("bg-gray-900");
+
   return (
-    <div className="min-h-screen bg-gray-900 text-gray-100 p-8">
+    <div className={`min-h-screen ${color} text-gray-100 p-8`}>
       <div className="max-w-6xl mx-auto">
+        <div className="flex flex-row space-x-1.5 mb-4">
+          <button onClick={() => setColor("bg-gray-900")}>
+            <Circle className="border-2 rounded-full" color="#111827" fill="#111827"/>
+          </button>
+          <button onClick={() => setColor("bg-green-300")}>
+            <Circle className="border-2 rounded-full" color="#86EFAC" fill="#86EFAC"/>
+          </button>
+          <button onClick={() => setColor("bg-rose-400")}>
+            <Circle className="border-2 rounded-full" color="#FB7185" fill="#FB7185"/>
+          </button>
+        </div>
         <div className="flex justify-between items-center mb-6">
-          <div className="flex items-center">
+          <div className="flex items-center bg-gray-800 rounded-lg px-4 py-2">
             <Link href="/f/1" className="text-gray-300 hover:text-white mr-2">
               My Drive
             </Link>
@@ -45,7 +59,7 @@ export default function GoogleDriveClone(props: {
             </SignedIn>
           </div>
         </div>
-        <div className="bg-gray-800 rounded-lg shadow-xl">
+        <div className="bg-gray-800 rounded-lg shadow-xl mb-5">
           <div className="px-6 py-4 border-b border-gray-700">
             <div className="grid grid-cols-12 gap-4 text-sm font-medium text-gray-400">
               <div className="col-span-6">Name</div>
